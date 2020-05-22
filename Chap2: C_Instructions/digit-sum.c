e64ba35ff0d8ca3d2516da952a5c1ca6fa10129d
Sayan De <sayande200@gmail.com>
2020-05-22 08:09:46 +0530

    Create digit-sum.c

---
+++ Chap2: C_Instructions/digit-sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a, a1,a2,a3,a4,a5;
+	printf("Enter Five digit number: ");
+	scanf("%d", &a);
+	a5=a%10;
+	a4=(a%100)/10;
+	a3=(a%1000)/100;
+	a2=(a%10000)/1000;
+	a1=(a%100000)/10000;
+	printf("The digits are: %d %d %d %d %d\n", a1,a2,a3,a4,a5);
+	printf("The sum of its digits are: %d\n", a1+a2+a3+a4+a5);
+	return 0;
+}
